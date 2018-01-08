776a231257bdb9a5e8ce701c24c2ad3d806f6233
MarcusJones <jones.0bj3@gmail.com>
2018-01-08 18:03:34 +0100

    Update, refactor to python normal

--- kaggle_titanic/titanic+r00.py
+++ kaggle_titanic/titanic+r00.py
@@ -1,73 +1,49 @@
+#===============================================================================
+#--- SETUP Config
+#===============================================================================
+from config.config import *
+import unittest
+
+#===============================================================================
+#--- SETUP Logging
+#===============================================================================
+import logging.config
+print(ABSOLUTE_LOGGING_PATH)
+logging.config.fileConfig(ABSOLUTE_LOGGING_PATH)
+myLogger = logging.getLogger()
+myLogger.setLevel("DEBUG")
+
+#===============================================================================
+#--- SETUP Standard modules
+#===============================================================================
+
+#===============================================================================
+#--- SETUP external modules
+#===============================================================================
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-#get_ipython().magic('matplotlib inline')
-
-from openpyxl import load_workbook
-
+import openpyxl as openpyxl
 import seaborn as sb
 
-excel_path = r"C:\EXPORT\titanic.xlsx"
+#===============================================================================
+#--- SETUP Custom modules
+#===============================================================================
+from ExergyUtilities.util_inspect import get_self
 
 
-# In[69]:
 
 
-# get titanic & test csv files as a DataFrame
-titanic_df = pd.read_csv("./data/train.csv")
-test_df    = pd.read_csv("./data/test.csv")
 
-small_df = titanic_df.iloc[:5]
-# preview the data
-titanic_df.head()
+#===============================================================================
+#--- MAIN CODE
+#===============================================================================
 
-
-# **Write the original data**
-
-# In[70]:
-
-
-writer = pd.ExcelWriter(excel_path, engine='xlsxwriter')
-#with writer: 
-titanic_df.to_excel(writer, sheet_name='original')
-writer.save()
-
-
-# In[71]:
-
-
-small_df
-
-
-# Variable|Definition|Key
-# -		|-|-
-# Survived|Survival|0 = No, 1 = Yes
-# Pclass	|Ticket class|1 = 1st, 2 = 2nd, 3 = 3rd
-# Name	| 
-# Sex		|Sex
-# Age		|Age in years
-# SibSp	|# of siblings / spouses aboard the Titanic
-# Parch	|# of parents / children aboard the Titanic
-# Ticket	|Ticket number	
-# Fare	|Passenger fare
-# Cabin	|Cabin number
-# Embarked|Port of Embarkation|C = Cherbourg, Q = Queenstown, S = Southampton
-
-# In[72]:
-
-
-summary_df = pd.DataFrame(titanic_df.describe())
-summary_df
-#print(titanic_df.describe())
-
-
-# In[73]:
-
-
-info_df = pd.DataFrame(titanic_df.info())
-
-
-# In[74]:
+#--- Set Pandas options
+pd.set_option('display.height', 500)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 
 def get_title(name):
@@ -92,120 +68,155 @@ def title_map(title):
     else:
         return 2
 
+def run(excel_path):
+    #--- Get DFs, full and test
+    titanic_df = pd.read_csv("./data/train.csv")
+    titanic_df.name = 'Titanic data'
+    logging.debug("Loaded {}, {} rows".format(titanic_df.name, titanic_df.shape))
+    
+    test_df    = pd.read_csv("./data/test.csv")
+    test_df.name = 'Titanic test data'
+    logging.debug("Loaded {}, {}".format(test_df.name, test_df.shape))
+    
+    #--- Write original to Excel sheet
+    if 0:
+        writer = pd.ExcelWriter(excel_path, engine='xlsxwriter')
+        titanic_df.to_excel(writer, sheet_name='original')
+        writer.save()
+    #write_orig_excel()
+    
+    #--- Print summary DF
+    if 0:
+        print("\n*** SUMMARY DF ***")
+        summary_df = pd.DataFrame(titanic_df.describe())
+        print(summary_df)
+        print("\n")
+        
+    #--- Print info DF
+    if 0:
+        print("\n*** INFO DF ***")
+        this_df = pd.DataFrame(titanic_df.info())
+        print(this_df)
+        print("\n")
+        # Some 'age' elements are missing
+        # Many 'cabin' elements are MISSING
+        # A couple 'embarked' are missing
+    
+    print(titanic_df['Embarked'])
+    
+    # Select Embarked = 
+    #new = this_df['Embarked'] == None
+    #print(new)
+    
+    raise
+    # In[74]:
+    
+    # **Split the TITLE Mr / Master / Ms / Mlle / Miss **
+    
+    # In[75]:
+    
+    
+    # Get the Name column. 
+    # For each element, get the title by checking for '.' then SPLIT on ','
+    # Return the series to a new column called 'title' in the DF
+    titanic_df['title'] = titanic_df['Name'].apply(get_title)
+    titanic_df[['PassengerId','Name','title']].head()
+    
+    
+    # In[76]:
+    
+    
+    # Use a case/swtich function to map class to an integer 1-5
+    titanic_df['title_int'] = titanic_df['title'].apply(title_map)
+    titanic_df.head()
+    
+    
+    # In[77]:
+    
+    
+    book = openpyxl.load_workbook(excel_path)
+    writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
+    writer.book = book
+    titanic_df.to_excel(writer, sheet_name='Split title')
+    writer.save()
+    writer.close()
+    
+    
+    # In[ ]:
+    
+    
+    #title_xt = pd.crosstab(titanic_df['title'], titanic_df['Survived'])
+    #title_xt_pct = title_xt.div(title_xt.sum(1).astype(float), axis=0)
+    #
+    #title_xt_pct.plot(kind='bar', 
+    #                  stacked=True, 
+    #                  title='Survival Rate by title')
+    #plt.xlabel('title')
+    #plt.ylabel('Survival Rate')
+    
+    
+    # In[ ]:
+    
+    
+    # We have to temporarily drop the rows with 'NA' values
+    # because the Seaborn plotting function does not know
+    # what to do with them
+    #fig = sb.pairplot(titanic_df.dropna());
+    
+    
+    # In[15]:
+    
+    
+    #fig.savefig(r"C:\EXPORT\titanic pair plot.png")
+    
+    
+    # In[20]:
+    
+    
+    
+    
+    
+    # Original data
+    
+    # In[23]:
+    
+    
+    writer = pd.ExcelWriter(r"C:\EXPORT\titanic.xlsx")
+    titanic_df.to_excel(writer,'Sheet5')
+    writer.save()
+    
+    
+    # Split names
+    
+    # In[31]:
+    
+    
+    df = small_df
+    df
+    
+    
+    # In[35]:
+    
+    
+    # Get the name col
+    df['Name']
+    # OR: 
+    df.Name
+    
+    
+    # In[55]:
+    
+    
+    #Get the split names as a 2D table
+    split_names = df.Name.str.split(',',n=1,expand=False).tolist()
+    #Put this into a new DF
+    new = [row[1] for row in split_names]
+    
+    df2 = pd.DataFrame(df.row.str.split(' ',1).tolist(),
+                                       columns = ['flips','row'])
+    
 
-# **Split the TITLE Mr / Master / Ms / Mlle / Miss **
+if __name__ == "__main__":
+    excel_path = r"C:\EXPORT\titanic.xlsx"
 
-# In[75]:
-
-
-# Get the Name column. 
-# For each element, get the title by checking for '.' then SPLIT on ','
-# Return the series to a new column called 'title' in the DF
-titanic_df['title'] = titanic_df['Name'].apply(get_title)
-titanic_df[['PassengerId','Name','title']].head()
-
-
-# In[76]:
-
-
-# Use a case/swtich function to map class to an integer 1-5
-titanic_df['title_int'] = titanic_df['title'].apply(title_map)
-titanic_df.head()
-
-
-# In[77]:
-
-
-book = load_workbook(excel_path)
-writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
-writer.book = book
-titanic_df.to_excel(writer, sheet_name='Split title')
-writer.save()
-writer.close()
-
-
-# In[ ]:
-
-
-#title_xt = pd.crosstab(titanic_df['title'], titanic_df['Survived'])
-#title_xt_pct = title_xt.div(title_xt.sum(1).astype(float), axis=0)
-#
-#title_xt_pct.plot(kind='bar', 
-#                  stacked=True, 
-#                  title='Survival Rate by title')
-#plt.xlabel('title')
-#plt.ylabel('Survival Rate')
-
-
-# In[ ]:
-
-
-# We have to temporarily drop the rows with 'NA' values
-# because the Seaborn plotting function does not know
-# what to do with them
-#fig = sb.pairplot(titanic_df.dropna());
-
-
-# In[15]:
-
-
-#fig.savefig(r"C:\EXPORT\titanic pair plot.png")
-
-
-# In[20]:
-
-
-
-
-
-# Original data
-
-# In[23]:
-
-
-writer = pd.ExcelWriter(r"C:\EXPORT\titanic.xlsx")
-titanic_df.to_excel(writer,'Sheet5')
-writer.save()
-
-
-# Split names
-
-# In[31]:
-
-
-df = small_df
-df
-
-
-# In[35]:
-
-
-# Get the name col
-df['Name']
-# OR: 
-df.Name
-
-
-# In[55]:
-
-
-#Get the split names as a 2D table
-split_names = df.Name.str.split(',',n=1,expand=False).tolist()
-#Put this into a new DF
-new = [row[1] for row in split_names]
-new
-
-
-# In[57]:
-
-
-df2 = pd.DataFrame(df.row.str.split(' ',1).tolist(),
-                                   columns = ['flips','row'])
-
-
-# In[ ]:
-
-
-# See regex help: 
-# https://regexr.com/
-
+    run(excel_path)
